a7decf8d93787d6a6901193b359708144f8ae03d
Houssem <ahmedhoussemayachi@gmail.com>
2026-06-30 14:01:10 +0200

    added redis and postgres to the compose file

---
+++ src/data.ts
@@ -0,0 +1,9 @@
+type SUPPORTED_GAME = "pacman"|"tetris"|"snake"|"breakout"|"donkeykong";
+
+export const max_scores: Record<SUPPORTED_GAME, number> = {
+    "pacman":   999999,
+    "tetris": 9999999,
+    "snake":    99999,
+    "breakout":   896980,
+    "donkeykong": 1247700,
+}

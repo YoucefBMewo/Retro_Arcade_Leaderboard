20108a43f313602e719c68fb68f1b3918a0112ef
YoucefBMewo <youcef.bouhzam@mewo-campus.fr>
2026-06-30 15:59:42 +0200

    correction fichier jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    // Redirige tous les imports .js vers leur équivalent sans extension
+    // Jest trouvera automatiquement le .ts correspondant
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   testMatch: ["**/tests/**/*.test.ts"],
   transform: {
     "^.+\\.ts$": ["ts-jest", {
